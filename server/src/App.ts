7a924d9c470c33d95713ed718870b5a77a8caa69
Silviu <silviu@astorian.com>
2020-05-28 15:23:11 +0300

    Add graphql

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,9 +1,27 @@
 import express from "express";
+import { buildSchema } from "graphql";
+import graphqlHTTP, { Middleware } from "express-graphql";
 
 const server = express();
 server.get('/', (req, res) => {
     res.send("Alive");
 });
+
+const schema = buildSchema(`
+    type Query{
+        hello: String
+    }
+`)
+
+const root = {
+    hello: () => "aaaa"
+}
+
+server.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
 
 server.listen(3000, (error) => {
     if (error) {
